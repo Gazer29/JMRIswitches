7abadfb9e329a429c03b7cfae48f110904feab81
Gazer29 <gazer29@hotmail.com>
2021-04-29 14:01:44 +0100

    Fixed FindSwitches

--- JMRIswitch.lua
+++ JMRIswitch.lua
@@ -1,40 +1,108 @@
 local component = require("component")
 local internet = require("internet")
+local serial = require("serialization")
+local fs = require("filesystem")
 local term = require ("term")
 local json = require("json")
+local CONFIG_FILE = "/home/settings.cfg"
+local SWITCH_TABLE = "/home/switchtable.tbl"
+local CONFIG = {ip = "84.65.32.30",port = "12080"}
+local DEFAULT_CONFIG = {ip = "84.65.32.30",port = "12080"}
+local getip = "http://"..CONFIG.ip..":"..CONFIG.port.."/json"
 local world = component.world_link
 local TcpIP = "84.65.32.30"
-local TcpSendPort = 6668
+local TcpSendPort = 12080
 local wait = 10
  
-function sendtcp(tosend)
-    if (con) then                
-        con:write("#04,"..tosend.."\r\n")
-        con:flush()
-        end
-end
- 
-function recvtcp()
-    data = nil
-    local decoded = nil
-    if (con) then 
-        index = con:read(10)
-        length = tonumber(index)
-        data = con:read(length)
-        if data ~= nil then
-            decoded = json:decode(data)
-            end
-        con:write("#0Q,\r\n")
-        con:flush()
-        end
+local function httpGET(ip)
+    local atable = {}
+    local decoded = ""
+    local result = ""
+    local handle = internet.request(ip,"",{},"GET")
+    if handle == nil then
+        print("failed to connect")
+    else
+        local mt = getmetatable(handle)
+        local code, message, headers = mt.__index.response()
+        --if code == 200 then
+        l = json:encode(headers)
+        n = "Code: "..(tostring(code))..", message: "..(tostring(message))..", headers: "..l
+        print(n)
+        print("-------")
+        for chunk in handle do result = result .. chunk end
+        print(result)
+        decoded = json:decode(result)
+    end
     return decoded
 end
+
+local function httpPUT(ip, data)
+    local atable = {}
+    local decoded = ""
+    local encoded = json:encode(data)
+    local header = {["Content-Type"] = "application/json;charset=utf-8"}
+    local result = ""
+    local handle = internet.request(ip, data, header, "PUT")
+    if handle == nil then
+        print("failed to connect")
+    else
+        local mt = getmetatable(handle)
+        local code, message, headers = mt.__index.response()
+        --if code == 200 then
+        l = json:encode(headers)
+        n = "Code: "..(tostring(code))..", message: "..(tostring(message))..", headers: "..l
+        print(n)
+        for chunk in handle do result = result .. chunk end
+        print(result)
+    end
+end
+
+local function httpPOST(ip, data)
+    local encoded = json:encode(data)
+    local header = {["Content-Type"] = "application/json;charset=utf-8"}
+    local handle = internet.request(ip, encoded, header, "POST")
+    if handle == nil then
+        print("failed to connect")
+    end
+end
  
+function saveFile(file, data)
+    local f = io.open(file, "w")
+    if f == nil then error("Couldn't open " .. file .. " to write config.") end
+    f:write(serial.serialize(data, 100000))
+    f:close()
+end
+
+function loadFile(file)
+    local f = io.open(file, "r")
+    if f == nil then 
+      print("Could not open " .. CONFIG_FILE .. ".")
+      return nil
+      else
+      local data = serial.unserialize(f:read("*a"))
+      f:close()
+      return data
+      end
+end
+
+function loadConfig(file)
+    local f = io.open(file, "r")
+    if f == nil then 
+      print("Could not open " .. CONFIG_FILE .. ".")
+      print("Loading default config")
+      return DEFAULT_CONFIG
+      else
+      local data = serial.unserialize(f:read("*a"))
+      f:close()
+      return data
+      end
+end
+
 function changeRed(data)
     for i, redbox in pairs(data) do
-        x = redbox.posit_array.x
-        y = redbox.posit_array.y
-        z = redbox.posit_array.z
+        x = redbox.position.x
+        y = redbox.position.y
+        z = redbox.position.z
         location = world.getLocationByCoordinates(x,y,z)
         a = location.getTileEntities().whereProperty("type", "automation:redstone_box").asList()
         if a ~= nil then
@@ -44,11 +112,35 @@ function changeRed(data)
             print("Set: ",x,y,z,"To: ",output)
             end
         end
+    
+end
+
+local function compareWeb(CurrSwitches, WebSwitches)
+    for name, data in pairs(CurrSwitches) do
+        if WebSwitches[name] == nil then
+            HTTPPUT(buildTurnout(name,name, data.state))
+        end
     end
- 
-function overArray()
-    local b = {}
-    local data = nil
+end
+
+local function compareTables(compare)
+    against = loadFile(SWITCH_TABLE)
+    if against ~= nil then
+        for name, data in pairs(compare) do
+            if against[name] == nil then
+                against[name] = data
+            end
+        end
+        saveFile(SWITCH_TABLE, against)
+        return against
+    else
+        return compare
+        saveFile(SWITCH_TABLE, compare)
+    end   
+end
+
+local function FindSwitches()
+    data = {}
     count = 0
     Allredboxes = world.getLoadedTileEntities().whereProperty("type", "automation:redstone_box").asList()
     if Allredboxes then
@@ -60,34 +152,103 @@ function overArray()
             c = {["x"] = xpos, ["y"] = ypos, ["z"] = zpos}
             output = redbox.getAPI("automation:redstone_box").getPowerLevel()
             a = {}
-            a["posit_array"] = c
-            a["output"] = output
-            table.insert(b,1,a)
+            a["position"] = c
+            a["state"] = Rstate(output)
+            name = tostring(xpos)..","..tostring(ypos)..","..tostring(zpos)
+            data[name] = a
             count = count + 1
             os.sleep(0.1)
             end
         end
-    data = json:encode(b)
-    if data == "" then
+    if data == {} then
         data = nil
         end
+    print("Redboxes found: ",count)
     return data
 end
  
+function ParseLight(x)
+    name = x["data"]["name"]
+    state = x["data"]["state"]
+    return Jstate(state)
+end
+
+local function ParseTurnout(x)
+    table = {}
+    for i,v in pairs(x) do
+        name = v["data"]["name"]
+        name = string.sub( name, 3 )
+        username = v["data"]["userName"]
+        comment = v["data"]["comment"]
+        inverted = v["data"]["inverted"]
+        state = Jstate(v["data"]["state"])
+        --print("Name: ",name,", Inverted: ", inverted,", State: ", state)
+        if name ~= nil then
+            table[name] = state
+        end
+    end
+    return table
+end
+
+local function buildTurnout(name, comment, state)
+    setstate = 4
+    if state == true then setstate = 2 end
+    data = {
+        "type"="turnout",
+        "data"={
+            "name"= "IT"..name,
+            "comment"= comment,
+            "inverted"= false,
+            "state"= setstate
+        }
+    }
+    return data
+end
+
+local function buildLight(name, state)
+    setstate = 4
+    if state == true then setstate = 2 end
+    data = {
+        "type"="light",
+        "data"={
+            "name"= name,
+            "state"= setstate
+        }
+    }
+    return data
+end
+
+local function Jstate(x) -- JMRI state to true or false
+    local out = false
+    if x == 15 then
+        out = true
+    end
+    return out
+end
+
+local function Rstate(x) -- Redstone state to true or false
+    local out = false
+    if x == 2 then
+        out = true
+    end
+    return out
+end
+
 -- MAIN --
- 
+
 while true do
-    xdata = overArray()
-    if xdata ~= nil then
-        con = internet.open(TcpIP,TcpSendPort)
-        sendtcp(xdata)
-        response = recvtcp()
-        end
-    if response ~= nil then
-        changeRed(response)
-        end
     print(os.date(" %I:%M %p"))
-    print("Redboxes sent: ", count)
+
+    if ParseLight(HTTPGET(getip.."/light/ILFindSwitches")) then
+        -- Set Web FindSwitches to false
+        HTTPPOST(getip.."/light/ILFindSwitches", buildLight("ILFindSwitches", false))
+        print("Finding Switches")
+        CurrSwitches = compareTables(FindSwitches())
+        compareWeb(CurrSwitches, (ParseTurnout(HTTPGET(getip.."/turnout")))
+    end
+    if ParseLight(HTTPGET(getip.."/light/ILUpdateSwitches")) then
+        UpdateSwitches()
+    end
     print("-----")
     os.sleep(wait)
 end
